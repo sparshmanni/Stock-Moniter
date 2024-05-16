d0a9bff300695e1f58b42d9d6ec97ad983e1f78b
Sparsh Manni <105648172+sparshmanni@users.noreply.github.com>
2024-05-17 00:15:34 +0530

    Update testapi.py

--- stock/testapi.py
+++ stock/testapi.py
@@ -1,7 +1,7 @@
 import requests
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=OR2F4lYDDHTDLTMS'
+url = 'https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=APIIIIKEYYY'
 r = requests.get(url)
 data = r.json()
 
